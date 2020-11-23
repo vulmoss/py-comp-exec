b9e63298e38b3f484851f5106491c250fd9bfe27
yaohongdi <yaokevin1987@gmail.com>
2020-11-23 23:15:46 +0800

    添加循环到做题中

--- base_study/姚帅是否被揍之算数篇.py
+++ base_study/姚帅是否被揍之算数篇.py
@@ -4,14 +4,18 @@
 
 import random
 
-print('这是一个20之内的随机练习')
-a = random.randint(1,20)
-print('a = {0}'.format(a))
-b = random.randint(1,a)
-print('b = {0}'.format(b))
-m = a - b
-c = int(input("输入你算数的{0} - {1} 的结果 :".format(a,b)))
-if c == m:
-    print("你的对的，结果是{0}🌹🌹🌹🌹 :".format(m))
-else :
-    print("你要加油哦💪")
+for i in range(0,10):
+    print('这是一个20之内的随机练习')
+    a = random.randint(1,20)
+    print('a = {0}'.format(a))
+    b = random.randint(1,a)
+    print('b = {0}'.format(b))
+    m = a - b
+    c = int(input("输入你算数的{0} - {1} 的结果 :".format(a,b)))
+    l = 0
+    if c == m:
+        print("你的对的，结果是{0}🌹🌹🌹🌹 :".format(m))
+        l += 1
+    else :
+        print("你要加油哦💪")
+print("你答对了{0}道题".format(l))
