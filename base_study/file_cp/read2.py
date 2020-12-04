36d96303f021ce060739cb5d9c61e04d3b2cce1c
yaohongdi <yaokevin1987@gmail.com>
2020-12-04 20:10:21 +0800

    operation the file

---
+++ base_study/file_cp/read2.py
@@ -0,0 +1,3 @@
+file_name= input("Please input the file name:")
+with open(file_name,'r') as file2:
+    print(file2.readlines())
---
+++ base_study/file_cp/write.py
@@ -0,0 +1,4 @@
+file_name = input("Plwase input the file :")
+with open(file_name,'w') as file1:
+    file1.write('testline1')
+    file1.write('testline2')
