dda3dbe206e4de82530fc67dfaac81048246638a
yaohongdi <yaokevin1987@gmail.com>
2021-01-21 11:36:37 +0800

    html url bs64解压

--- code_fun/easy_crack1/crack1.py
+++ code_fun/easy_crack1/crack1.py
@@ -2,6 +2,7 @@
 # encoding: utf-8
 #__author__ == 'vulMoss'
 
+import base64
 import html
 from urllib.parse import unquote
 
@@ -9,4 +10,8 @@ file1 = 'pwd.txt'
 with open(file1) as f1:
     r1 = f1.read()
     r2 = html.unescape(r1)
-    print(r2)
+#    print(r2)
+    r3 = unquote(r2)
+#    print(r3)
+    result = base64.b64decode(r3)
+    print(result.decode())
