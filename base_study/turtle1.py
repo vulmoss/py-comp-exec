80634a2bb3b80bf2066d595ca20c356a5f9fd349
yaohongdi <yaokevin1987@gmail.com>
2021-06-07 17:53:13 +0800

    turtle test

---
+++ base_study/turtle1.py
@@ -0,0 +1,11 @@
+# !/usr/bin/env python3
+# -*-coding:utf-8-*-
+# __author__ == vulMoss
+
+import turtle as t
+for i in range(2):
+    t.forward(100)
+    t.right(60)
+    t.backward(100)
+    t.right(60)
+t.done()
