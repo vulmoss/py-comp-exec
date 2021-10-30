04901f0b44ee371e603cc19124d258e6e8b2f852
yaohongdi <yaokevin1987@gmail.com>
2021-10-30 19:38:04 +0800

    modify 3sum_2

--- leetcode/3nums_2.py
+++ leetcode/3nums_2.py
@@ -6,25 +6,27 @@ class Solution(object):
     def threeSum(self,nums):
         n = len(nums)
         res = []
-        if (not nums or n < 3):
+        if(not nums or n<3):
             return []
         nums.sort()
         res = []
         for i in range(n):
             if(nums[i]>0):
                 return res
-            if(i>0 and nums[i]==nums[i-1]):
+            if(i>0 and nums[i] == nums[i-1]):
                 continue
             L = i+1
-            R = n+1
+            R = n-1
             while(L<R):
-                if(nums[i] + nums(L) + nums(R) ==0):
+                if(nums[i]+nums[L]+nums[R] == 0):
                     res.append([nums[i],nums[L],nums[R]])
                     while(L<R and nums[L] == nums[L+1]):
                         L += 1
-                    while(L<R and nums[R] == nums[R-1]):
+                    while(L<R and nums[R] ==nums[R-1]):
                         R -= 1
-                elif(nums[i] + nums(L) + nums(R) > 0):
+                    L += 1
+                    R -= 1
+                elif(nums[i]+nums[L]+nums[R]):
                     R -= 1
                 else:
                     L += 1
