435eb95b9bbabf62ec5c94a7f41cd0e251e160ad
vulmoss <yaokevin1987@gmail.com>
2022-01-31 01:07:21 +0800

    为leetcode53前缀和的算法添加注释

--- leetcode/leetcode_53/前缀和_sums.py
+++ leetcode/leetcode_53/前缀和_sums.py
@@ -5,8 +5,8 @@ class Solution:
         minsum = n_sums =0
         for i in range(n):
             n_sums += nums[i]
-            maxsum = max(maxsum,n_sums - minsum)
-            minsum = min(n_sums,minsum)
+            maxsum = max(maxsum,n_sums - minsum)#只保留住最大的那个结果，既前缀之和最大的
+            minsum = min(n_sums,minsum)#算法的精髓所在，加上了新的元素对应的数值之后，是否比以前更小，
         return maxsum
 
 
@@ -17,3 +17,8 @@ if __name__ == '__main__':
     ms = s.maxSubArray
     nums = [-1,2,3,4,-1]
     print(ms(nums))
+'''
+0.....i....j.....
+最大的数组的排列是从i到j  前缀和：j的前缀和-i的前缀和最大
+整体来说就是不断的计算前缀和的差
+'''
