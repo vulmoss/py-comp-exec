233d0526c001c0eb3ad6edb074d6df20663076d1
vulmoss <yaokevin1987@gmail.com>
2022-01-30 14:21:12 +0800

    leetcode53为动态规划方法添加注释

--- leetcode/leetcode_53/dynamic_sums.py
+++ leetcode/leetcode_53/dynamic_sums.py
@@ -1,10 +1,10 @@
 class Solution:
     def maxSubArray(self, nums):
         n = len(nums)
-        max_ending_index = maxsum =nums[0]
-        for i in range(1,n):
-            max_ending_index = max(max_ending_index + nums[i],nums[i])
-            maxsum = max(max_ending_index,maxsum)
+        max_ending_index = maxsum =nums[0] #第一个元素初始化
+        for i in range(1,n): #从1开始循环
+            max_ending_index = max(max_ending_index + nums[i],nums[i]) #临时的最大值判断添加了i的元素的值后 ，时候比i的值大，大的话临时的最大值就给
+            maxsum = max(max_ending_index,maxsum)#最大值和临时最大值进行比较。
         return  maxsum
 
 
@@ -13,3 +13,6 @@ if __name__ == '__main__':
     ms = s.maxSubArray
     nums = [-1,2,3,4,-1]
     print(ms(nums))
+ '''
+ 动态规划的思想：Q(list,i)  = max(0,Q(list -1)) + nums[i]
+ '''
