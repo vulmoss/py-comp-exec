0f808f1b6d33635b33ecc60def851b24d6da6794
yaohongdi <yaokevin1987@gmail.com>
2022-09-30 21:08:33 +0800

    proxy study

--- code_fun/python_black_hat/proxy/p_v1.py
+++ code_fun/python_black_hat/proxy/p_v1.py
@@ -17,3 +17,12 @@ def server_loop(local_host,local_port,remote_host,remote_port,receive_first):
         print("[+] Failed to listen on %s:%d" %(local_host,local_port))
         print("[+] Check for other listening sockets or correct permissions.")
         sys.exit(0)
+    print("[*] listening on %s:%d" %(local_host,local_port))
+
+    server.listen(5)
+
+    while True:
+        client.socket,addr = server.accept()
+        print("[==>] Received incoming connection for %s:%d" %(addr[0],addr[1]))
+        proxy_thread = threading.Thread(target=proxy_h)
+
