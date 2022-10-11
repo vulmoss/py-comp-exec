9d7794c4bc02380852bfa8968edd192663f908dc
vulmoss <yaokevin1987@gmail.com>
2022-10-11 17:57:55 +0800

    20221011

--- code_fun/python_black_hat/proxy/p_v1.py
+++ code_fun/python_black_hat/proxy/p_v1.py
@@ -17,3 +17,12 @@ def server_loop(local_host,local_port,remote_host,remote_port,receive_first):
         print("[+] Failed to listen on %s:%d" %(local_host,local_port))
         print("[+] Check for other listening sockets or correct permissions.")
         sys.exit(0)
+    print("[*] Listening on %s:%d" %(local_host,local_port))
+
+    server.listen(5)
+
+    while True:
+        client_socket,addr = server.accept() #等待传入连接。 返回代表连接的新套接字【IP套接字，地址信息：（主机地址，端口）】以及客户端的地址
+        print("[==>] Received incoming connection from %s:%d" %(addr[0],addr[1]))
+
+
