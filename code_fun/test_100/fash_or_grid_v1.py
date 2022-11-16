aa6b42a83b8cbb2c811dbf4045531aa693c064ef
yaohongdi <yaokevin1987@gmail.com>
2022-11-16 21:05:10 +0800

    the code v1

--- code_fun/test_100/fash_or_grid_v1.py
+++ code_fun/test_100/fash_or_grid_v1.py
@@ -7,3 +7,42 @@
 # @Software : PyCharm
 
 
+def runYear(year):
+    if (year % 4 == 0 and year % 100 !=0) or (year % 400 == 0):
+        return 1
+    else :
+        return 0
+
+def countDay(currentDay):
+    perMonth = [0,31,28,31,30,31,30,31,31,30,31,30]
+    totalDay=0
+    year = 1990
+    while year < currentDay('year'):
+        if runyear(year) == 1:
+            totalDay  = totalDay + 366
+        else:
+            totalDay = totalDay + 365
+        year += 1
+    if runYear(currentDay['year']) == 1:
+        perMonth[2] += 1
+    i = 0
+    while i < currentDay['month']:
+        totalDay += perMonth[i]
+        i += 1
+    totalDay += currentDay['day']
+    return totalDay
+
+def results_day():
+    print("pleae input the year , month,day")
+    year,month,day = [int(i) for i in input().split()]
+    today = {'year':year,'month':month,'day':day}
+    totalDay = countDay(today)
+    print("%dyear%dmonth%dday-19900101 %days" %(year,month,day,totalDay))
+    result = totalDay %5
+    if result >0 and result <4:
+        print("today fash")
+    else:
+        print("grid")
+
+if __name__ == "__main__":
+    results_day()
