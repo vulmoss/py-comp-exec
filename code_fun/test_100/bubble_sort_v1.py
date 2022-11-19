785f13163e073917228f07a5aa2838825347d22c
yaohongdi <yaokevin1987@gmail.com>
2022-11-19 14:43:04 +0800

    the v1 code bubble sort

--- code_fun/test_100/bubble_sort_v1.py
+++ code_fun/test_100/bubble_sort_v1.py
@@ -11,3 +11,27 @@
 对N个整数（数据由键盘输入）进行升序排列。
 """
 
+def bubbleSort(a):
+    n = len(a)
+    i = 1
+    while i <= n-1:
+        j = 0
+        while j < n - i:
+            if a[j] > a[j+1]:
+                t = a[j]
+                a[j] = a[j+1]
+                a[j+1] = t
+            j += 1
+        i += 1
+    for a1 in a:
+        print(a1,end= " ")
+
+def resultSort():
+    x = input()
+    a = x.split(" ")
+    for i in range(0,len(a)):
+        a[i] = int(a[i])
+    bubbleSort(a)
+
+if __name__ == "__main__":
+    resultSort()
