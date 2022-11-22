16d2397fe5ed1756e5cd976096547ec44ef00547
yaohongdi <yaokevin1987@gmail.com>
2022-11-22 08:52:47 +0800

    add the numbers system converions

---
+++ code_fun/test_100/num_system_conversion_v1.py
@@ -0,0 +1,7 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+# @Time     : 2022/11/22 08:52
+# @Author   : VulMoss
+# @Site     : 
+# @File     : num_system_conversion_v1.py
+# @Software : PyCharm
