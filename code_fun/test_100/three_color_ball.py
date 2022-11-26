4855aaec27f063b3e432e1ccaee2184ff6ec4e75
yaohongdi <yaokevin1987@gmail.com>
2022-11-26 21:40:08 +0800

    add the code for three color

--- code_fun/test_100/three_color_ball.py
+++ code_fun/test_100/three_color_ball.py
@@ -9,3 +9,16 @@
 """
 一个口袋中放有12个球，已知其中3个是红的，3个是白的，6个是黑的，现从中任取8个，问共有多少种可能的颜色搭配？
 """
+
+def ball():
+    print("\t rad \t whate \t block")
+    print("+++++++++++++++++++++++++++++++++++++++")
+    num = 0
+    for r in range(0,4): 
+        for w in range(0,4):
+            if 8 - r - w <=6:
+                num += 1
+                print("%2d: %d \t\t %d \t \t %d" %(num, r,w,8-r-w))
+
+if __name__ == "__main__":
+    ball()
