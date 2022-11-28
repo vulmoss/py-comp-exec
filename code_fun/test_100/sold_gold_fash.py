94b795c29f36607d6751c0cfd3a54470d0581a3f
yaohongdi <yaokevin1987@gmail.com>
2022-11-28 09:16:32 +0800

    添加卖金鱼的代码

--- code_fun/test_100/sold_gold_fash.py
+++ code_fun/test_100/sold_gold_fash.py
@@ -12,3 +12,24 @@
 第3次卖出余下的四分之一加1/4条；第4次卖出余下的五分之一加1/5条；最后卖出余下的11条。
 试编程求出原来鱼缸中共有多少条金鱼。
 """
+
+def fash():
+    flag = 0
+    i = 23
+    while flag == 0:
+        j = 1
+        x = i
+        while j<=4 and x >= 11:
+            if (x+1) %(j+1) == 0:
+                x -= (x+1)//(j+1)
+            else:
+                x = 0
+                break
+            j += 1
+        if j == 5 and  x ==11:
+            print("the fash have %d "%i)
+            flag = 1
+        i += 2
+
+if __name__ == "__main__":
+    fash()
