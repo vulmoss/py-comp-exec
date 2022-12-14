a5ba637a2d550b7bcd3704f99d67007b91cdb76d
yaohongdi <yaokevin1987@gmail.com>
2022-12-14 22:02:21 +0800

    add the max money solution

---
+++ code_fun/test_100/money_max.py
@@ -0,0 +1,16 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+# @Time     : 2022/12/14 21:57
+# @Author   : VulMoss
+# @Site     : 
+# @File     : money_max.py
+# @Software : PyCharm
+
+
+
+
+
+
+def money_max():
+    max = 0.0
+    
