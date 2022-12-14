8e8781d697ec318c233a03e848b25059c23f83ee
yaohongdi <yaokevin1987@gmail.com>
2022-12-14 22:52:08 +0800

    code v1

--- code_fun/test_100/money_max.py
+++ code_fun/test_100/money_max.py
@@ -21,4 +21,30 @@
 
 def money_max():
     max = 0.0
-    
+    for x8 in range(0,3):
+        t5 = (20-8*x8)//5
+        for x5 in (0,t5+1):
+            t3 = (20-8*x8-5*x5) //3
+            for x3 in range(0,t3+1):
+                t2 = (20-8*x8-5*x5-3*x3)//2
+                for x2 in (0,t2+1):
+                    x1 = 20-8*x8-5*x5-3*x3-2*x2
+                    result = 2000 * ((1+0.0063*12)**x1) *((1+2*0.0066*12)**x2) *((1+3*0.0069*12)**x3)\
+                             *((1+5*0.0075*12)**x5) * ((1+8*0.0084*12)**x8)
+                    if result > max:
+                        max =result
+                        y1 =x1
+                        y2=x2
+                        y3=x3
+                        y5=x5
+                        y8=x8
+    print("best soultion:")
+    print("8 year is %d" %y8)
+    print("5 year is %d" % y5)
+    print("3 year is %d" % y3)
+    print("2 year is %d" % y2)
+    print("1 year is %d" % y1)
+    print("end the money is %0.2f "%result)
+
+if __name__ =="__main__":
+    money_max()
