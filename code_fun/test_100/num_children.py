d2e2c5847a0ea61b3bf2dbce8940e6226e05bfb7
yaohongdi <yaokevin1987@gmail.com>
2022-12-17 23:46:58 +0800

    抽象出二元一次方程

---
+++ code_fun/test_100/num_children.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+# @Time     : 2022/12/17 23:37
+# @Author   : VulMoss
+# @Site     : 
+# @File     : num_children.py
+# @Software : PyCharm
+
+"""
+男孩看男孩和女孩一样多，女孩看男孩比女孩多一倍  男孩是x 女孩是y   x=y+1 (y-1)*2=x => y+1 = 2y -2 =>y=3 --->x=4
+"""
+
+
+
