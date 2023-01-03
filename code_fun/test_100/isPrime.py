938d6312c7afc9dfa51b7a01b2f2545e0275504a
yaohongdi <yaokevin1987@gmail.com>
2023-01-03 22:09:09 +0800

    判断输入是否为素数

---
+++ code_fun/test_100/isPrime.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+# @Time     : 2023/1/3 22:07
+# @Author   : VulMoss
+# @Site     : 
+# @File     : isPrime.py
+# @Software : PyCharm
+
+def isPrime():
+    pass
