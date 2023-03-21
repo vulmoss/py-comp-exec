1e0f71755f235389e321b107ebe40ec77700b6e9
yaohongdi <yaokevin1987@gmail.com>
2023-03-21 16:28:09 +0800

    candy init

--- code_fun/test_100/distribution_candy.py
+++ code_fun/test_100/distribution_candy.py
@@ -10,3 +10,22 @@
 第8个小孩6块，第9个小孩14块，第10个小孩20块。然后所有的小孩同时将手中的糖分一半给右边的小孩；糖块数为奇数的人可向老师要一块。
 问经过这样几次后大家手中的糖一样多？每人各有多少块糖？
 '''
+
+def judge(candy):
+    for i in range(0,10):
+        if candy[0] != candy[i]:
+            return 1
+    return 0
+
+def giveSweet(sweet,j):
+    t = [0] * 10
+    while (judge(sweet)):
+        for i in range(0,10):
+            if sweet[i] %2 == 0:
+                sweet[i] = sweet[i] //2
+                t[i] = sweet[i]
+            else:
+                sweet[i] = (sweet[i] + 1) //2
+                t[i] = sweet[i]
+        for n in range(0,9):
+            
