d544dc15c46ac8859f4ea78285fe128d722ae8a0
yaohongdi <yaokevin1987@gmail.com>
2023-04-02 14:01:36 +0800

    add the exchange money v1

---
+++ code_fun/test_100/exchange_money.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+# @Time     : 2023/4/2 13:59
+# @Author   : VulMoss
+# @Site     : 
+# @File     : exchange_money.py
+# @Software : PyCharm
+
+
+''''
+将5元的人民币兑换成1元、5角和1角的硬币，共有多少种不同的兑换方法。
+'''
