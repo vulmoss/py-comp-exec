2ae78fee59d1ece3d69cfa04977676b751a59ae8
yaohongdi <yaokevin1987@gmail.com>
2023-04-03 13:10:48 +0800

    finish the code with exchangeMoney

--- code_fun/test_100/exchange_money.py
+++ code_fun/test_100/exchange_money.py
@@ -9,7 +9,19 @@
 
 ''''
 将5元的人民币兑换成1元、5角和1角的硬币，共有多少种不同的兑换方法。
+x/y/z
 '''
 
 def exchangeMoney():
-    pass
+    number = 0
+    for x in range(0,51,10):
+        for y in range(0,51-x,5):
+            for z in range(0,51-x-y):
+                if x + y + z == 50:
+                    number += 1
+                    print("%2d: %2d %2d %2d" % (number, x//10, y//5, z))
+
+if __name__ == "__main__":
+    print(" the plan is ")
+    exchangeMoney()
+ 
