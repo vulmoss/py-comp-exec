b13ad695a037303abfaa32896ccdadff32c2157b
yaohongdi <yaokevin1987@gmail.com>
2023-11-01 09:13:18 +0800

    add the analysis

---
+++ code_fun/test_100/palindromeNumber.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+# @Time     : 2023/4/3 13:15
+# @Author   : VulMoss
+# @Site     : 
+# @File     : palindromeNumber.py
+# @Software : PyCharm
+
+'''
+打印所有不超过n（取n<256）的其平方具有对称性质的数（也称回文数）。
+数字转换成字符串，然后切片的方式，左右取出，并比较 [0:len(num)//2]
+'''
+
