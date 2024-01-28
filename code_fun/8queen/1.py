4ff824853691eaeda4b3bd1573de7215eb84075d
yaohongdi <yaokevin1987@gmail.com>
2024-01-28 21:03:45 +0800

    soultion 4 gueen v1

--- code_fun/8queen/1.py
+++ code_fun/8queen/1.py
@@ -6,7 +6,7 @@
 # @File     : 1.py
 # @Software : PyCharm
 
-def noConflicts(board,current,qindex,n): # current 列 #qindex 行索引
+def noConflicts(board,current,qindex,n): # current 列  #qindex 行索引
     for j in range(current):
         if board[qindex][j] == 1: #行是否有索引
             return False
@@ -19,5 +19,29 @@ def noConflicts(board,current,qindex,n): # current 列 #qindex 行索引
     while qindex + k < n and current - k >= 0:
         if board[qindex + k][current -k] == 1:
             return False
-        K += 1
+    k += 1
     return True
+def FourQueens(n=4):
+    board = [[0,0,0,0],[0,0,0,0],[0,0,0,0],[0,0,0,0]]
+    for i in range(n):
+        board[i][0] = 1
+        for j in range(n):
+            board[j][1] = 1
+            if noConflicts(board,1,j,n):
+                for k in range(n):
+                    board[k][2] = 1
+                    if noConflicts(board,2,k,n):
+                        for m in range(n):
+                            board[m][3] = 1
+                            if noConflicts(board,3,m,n):
+                                print(board)
+                            board[m][3] = 0
+                    board[k][2] = 0
+            board[j][1] =0
+        board[i][0] = 0
+    return
+
+
+if __name__ == '__main__':
+    a=FourQueens()
+    print(a)
