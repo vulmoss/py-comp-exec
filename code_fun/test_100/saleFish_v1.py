3bd7d930f38eec41f1558f1f74f3e0debeef8fbe
yaohongdi <yaokevin1987@gmail.com>
2024-04-06 00:02:08 +0800

    fix the bug soldfish

--- code_fun/test_100/saleFish_v1.py
+++ code_fun/test_100/saleFish_v1.py
@@ -14,17 +14,18 @@
 def soleFish():
     i = 23
     flag = 0
+    x = 23
     while flag == 0:
         j = 1
-        x = 1
+        x = i
         while j <= 4 and x >= 11:
-            if (x + 1) % (j + 1) ==0:
+            if (x + 1) % (j + 1) == 0:
                 x -= (x+1)//(j+1)
             else:
                 x = 0
                 break
             j += 1
-        if j ==5 and x == 11:
+        if j == 5 and x == 11:
             print("Total have %d fish." %i)
             flag = 1
         i += 2
