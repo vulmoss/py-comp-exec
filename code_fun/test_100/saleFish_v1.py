73f41888c9ce2a8b5daf4e998280959f3909cf32
yaohongdi <yaokevin1987@gmail.com>
2024-04-05 21:26:20 +0800

    first finish

--- code_fun/test_100/saleFish_v1.py
+++ code_fun/test_100/saleFish_v1.py
@@ -10,3 +10,24 @@
 小明将养的一缸金鱼分5次出售：第1次卖出全部的一半加1/2条；第2次卖出余下的三分之一加1/3条；第3次卖出余下的四分之一加1/4条；
 第4次卖出余下的五分之一加1/5条；最后卖出余下的11条。试编程求出原来鱼缸中共有多少条金鱼。
 """
+
+def soleFish():
+    i = 23
+    flag = 0
+    while flag == 0:
+        j = 1
+        x = 1
+        while j <= 4 and x >= 11:
+            if (x + 1) % (j + 1) ==0:
+                x -= (x+1)//(j+1)
+            else:
+                x = 0
+                break
+            j += 1
+        if j ==5 and x == 11:
+            print("Total have %d fish." %i)
+            flag = 1
+        i += 2
+
+if __name__ == "__main__":
+    soleFish()
